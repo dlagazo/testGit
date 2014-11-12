e51a0354ffe86f6c4eddf018e424245ec743f405
dlagazo <dlagazo@gmail.com>
2014-11-12 12:21:50 +0800

    Release 0.1

--- testGit/ViewController.swift
+++ testGit/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        println("Master 0.1")
+        println("Release 0.1")
     }
 
     override func didReceiveMemoryWarning() {
